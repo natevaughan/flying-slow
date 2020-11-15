eaeced18b0128fe500e3dce64c979dfcc0fa86ee
Nate Vaughan <nate.vaughan@protonmail.com>
2020-11-15 12:53:22 -0600

    show altitude

--- src/Aircraft.svelte
+++ src/Aircraft.svelte
@@ -17,11 +17,13 @@
         margin-bottom: 4px;
         font-family: "Courier New", Courier, fixed-width;
     }
+    .mb-4 {
+        margin-bottom: 16px;
+    }
 </style>
-<div class="row">
+<div class="row mb-4">
     <h3>{name}</h3>
     <div>400-NM Seats: {min([performanceData[0]["400NMSeats"], performanceData[0]["Seats"]])}</div>
-
-    <div>Econ cruise: {maxEcon.Speed} kts @  {maxEcon.Burn} gph <a href={maxEcon.Source}>(source)</a></div>
-    <div>Max cruise: {maxCruise.Speed} kts @ {maxCruise.Burn} gph <a href={maxCruise.Source}>(source)</a></div>
+    <div>Econ cruise: {maxEcon.Speed} kts,  {maxEcon.Burn} gph @ {maxEcon.Altitude || 'unknown' } ft <a href={maxEcon.Source}>(source)</a></div>
+    <div>Max cruise: {maxCruise.Speed} kts, {maxCruise.Burn} gph @ {maxCruise.Altitude || 'unknown' } ft <a href={maxCruise.Source}>(source)</a></div>
 </div>
